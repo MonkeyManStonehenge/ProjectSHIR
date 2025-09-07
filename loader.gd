6c4b9258aa0e51b39455518d94f335d16dbe973a
DuckTheDunce <rodentiaschizophrenia@gmail.com>
2025-09-06 22:13:28 -0600

    quit

--- loader.gd
+++ loader.gd
@@ -7,6 +7,8 @@ func message(sender, msg, data):
 			sender.visible = false
 			data.process_mode = Node.PROCESS_MODE_PAUSABLE
 			data.visible = true
+		"quit":
+			get_tree().quit()
 		_:
 			printerr("unrecognized message sent to loader")
 		
---
+++ quit_button.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var mouseover = false
+
+func _mouse_enter():
+	mouseover = true
+	
+func _mouse_exit(): 
+	mouseover = false
+	
+func _input(event: InputEvent):
+	if event is InputEventMouseButton and mouseover:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			get_node("/root/Main").message(self.get_parent(), "quit", null)
