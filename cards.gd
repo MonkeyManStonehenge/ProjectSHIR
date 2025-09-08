6e211a8e7f196cd9097df03777215959d02c52e9
DuckTheDunce <rodentiaschizophrenia@gmail.com>
2025-09-07 21:37:24 -0600

    wait is that stonehenge guy?

--- cards.gd
+++ cards.gd
@@ -5,3 +5,11 @@ func _ready():
 	
 func start(enemy:Texture2D):
 	get_child(0).texture = enemy
+
+func _input(event: InputEvent):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_node("/root/Main").message(null, "load", get_node("/root/Main/Menu"))
+			
+			await get_node("/root/Main/Menu").activeScene(self)
+			get_node("/root/Main").message(null, "pause", self)
